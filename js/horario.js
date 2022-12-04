e0d36170646c1360bb625285842a5eefeb55f39c
denisjeangop <102286862+denisjeangop@users.noreply.github.com>
2022-12-04 19:04:49 +0100

    cambios y arreglos

--- js/horario.js
+++ js/horario.js
@@ -1,12 +1,13 @@
 function horarioDia(){
-
-    document.write("<table border='1'><thead><tr><th>HORARIOS MAÑANAS</th><th>Lunes</th> <th>Martes</th> <th>Miercoles</th> <th>Jueves</th> <th>Viernes</th>  </tr> <tr>  <th>9:00 - 11:00</th>   <th></th> <td></td> <td></td><td></td><td></td><td></td></tr><tr><th>11:00 - 13:00</th><td></td> <td></td><td></td><td></td><td></td></tr><tr><th>13:00 - 15:00</th><td></td> <td></td><td></td><td></td><td></td></tr></thead><tbody></tbody></table>")    ;
-
+    let calendario = document.getElementById("calendario");
+    let valor = "<table border='1'><thead><tr><th>HORARIOS MAÑANAS</th><th>Lunes</th> <th>Martes</th> <th>Miercoles</th> <th>Jueves</th> <th>Viernes</th>  </tr> <tr>  <th>9:00 - 11:00</th>   <th></th> <td></td> <td></td><td></td><td></td><td></td></tr><tr><th>11:00 - 13:00</th><td></td> <td></td><td></td><td></td><td></td></tr><tr><th>13:00 - 15:00</th><td></td> <td></td><td></td><td></td><td></td></tr></thead><tbody></tbody></table>"
+    calendario.innerHTML = valor;
 }
 
 function horarioTarde(){
-    document.write("<table border='1'><thead><tr><th>HORARIOS TARDES</th><th>Lunes</th> <th>Martes</th> <th>Miercoles</th> <th>Jueves</th> <th>Viernes</th>  </tr> <tr>  <th>16:00 - 17:00</th>   <td></td> <td></td><td></td><td></td><td></td></tr><tr><th>17:00 - 18:00</th><td></td> <td></td><td></td><td></td><td></td></tr><tr><th>18:00 - 19:00</th><td></td> <td></td><td></td><td></td><td></td></tr><tr><th>19:00 - 20:00</th><td></td> <td></td><td></td><td></td><td></td></tr><tr><th>20:00 - 21:00</th><td></td> <td></td><td></td><td></td><td></td></tr></thead><tbody></tbody></table>")    ;
-
+  let calendario = document.getElementById("calendario"); 
+  let valor = "<table border='1'><thead><tr><th>HORARIOS TARDES</th><th>Lunes</th> <th>Martes</th> <th>Miercoles</th> <th>Jueves</th> <th>Viernes</th>  </tr> <tr>  <th>16:00 - 17:00</th>   <td></td> <td></td><td></td><td></td><td></td></tr><tr><th>17:00 - 18:00</th><td></td> <td></td><td></td><td></td><td></td></tr><tr><th>18:00 - 19:00</th><td></td> <td></td><td></td><td></td><td></td></tr><tr><th>19:00 - 20:00</th><td></td> <td></td><td></td><td></td><td></td></tr><tr><th>20:00 - 21:00</th><td></td> <td></td><td></td><td></td><td></td></tr></thead><tbody></tbody></table>";
+  calendario.innerHTML = valor;
 }
 /*
 function genera_tabla() {
